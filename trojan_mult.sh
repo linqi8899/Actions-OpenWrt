7e254e1d86a134cfe784115820f7a3837cb265cf
linqi8899 <61348827+linqi8899@users.noreply.github.com>
2020-03-24 00:05:19 +0800

    Update trojan_mult.sh

--- trojan_mult.sh
+++ trojan_mult.sh
@@ -191,14 +191,14 @@ EOF
 	wget https://github.com/trojan-gfw/trojan/releases/download/v${latest_version}/trojan-${latest_version}-linux-amd64.tar.xz
 	tar xf trojan-${latest_version}-linux-amd64.tar.xz
 	#下载trojan客户端
-	#wget https://github.com/atrandys/trojan/raw/master/trojan-cli.zip
+	wget https://github.com/atrandys/trojan/raw/master/trojan-cli.zip
 	wget -P /usr/src/trojan-temp https://github.com/trojan-gfw/trojan/releases/download/v${latest_version}/trojan-${latest_version}-win.zip
-	#unzip trojan-cli.zip
+	unzip trojan-cli.zip
 	unzip /usr/src/trojan-temp/trojan-${latest_version}-win.zip -d /usr/src/trojan-temp/
-	#cp /usr/src/trojan-cert/fullchain.cer /usr/src/trojan-cli/fullchain.cer
+	cp /usr/src/trojan-cert/fullchain.cer /usr/src/trojan-cli/fullchain.cer
 	mv -f /usr/src/trojan-temp/trojan/trojan.exe /usr/src/trojan-cli/ 
 	trojan_passwd=$(cat /dev/urandom | head -1 | md5sum | head -c 8)
-	#cat > /usr/src/trojan-cli/config.json <<-EOF
+	cat > /usr/src/trojan-cli/config.json <<-EOF
 {
     "run_type": "client",
     "local_addr": "127.0.0.1",
